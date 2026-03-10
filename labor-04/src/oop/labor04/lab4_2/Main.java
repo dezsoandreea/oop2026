205c5e942470a3f59ac0a7b18b2ed5648bde84a0
dezsoandreea <dezso.andreea@student.ms.sapientia.ro>
2026-03-10 22:27:47 +0200

    lab4,fel2

--- labor-04/src/oop/labor04/lab4_2/Main.java
+++ labor-04/src/oop/labor04/lab4_2/Main.java
@@ -9,21 +9,26 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Customer> customers = new ArrayList<>();
 
-        int bankaccountnumber=0;
-        int customernumber=0;
-
         try (Scanner scanner = new Scanner(new File("lab4_2_input.csv"))) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 if (line.isEmpty()) {
                     continue;
                 }
-                if(line.startsWith("Customer: ")) {
-                    System.out.println("This is a Customer");
-                    ++customernumber;
-                }else{
-                    System.out.println("This is a BankAccount");
-                    ++bankaccountnumber;
+                String firstName;
+                String lastName;
+                String[] items = line.split(",");
+                String tip = items[0].trim();
+                if (tip.equals("Customer")) {
+                    firstName = items[1].trim();
+                    lastName = items[2].trim();
+                    customers.add(new Customer(firstName, lastName));
+                }
+                if (tip.equals("Account")) {
+                    String accountNumber = items[1].trim();
+                    double balance = Double.parseDouble(items[2].trim());
+                    customers.getLast().addAccount(new BankAccount(accountNumber));
+                    customers.getLast().getAccount(accountNumber).deposit(balance);
                 }
             }
         } catch (FileNotFoundException e) {
