0da4e92068bad8336829a8c9533399d508c158c3
dezsoandreea <dezso.andreea@student.ms.sapientia.ro>
2026-05-17 15:45:42 +0300

    lab12, fel1

--- labor-12/src/oop/labor12/lab12_1/Main.java
+++ labor-12/src/oop/labor12/lab12_1/Main.java
@@ -4,36 +4,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int dictionaryType = scanner.nextInt();
-        switch (dictionaryType) {
-            case 1: {
-                long startTime = System.nanoTime();
-                DictionaryService service = new DictionaryService(DictionaryType.ARRAY_LIST);
-                service.findWordsFile("bible.txt");
-                long endTime = System.nanoTime();
-                long timeElapsed = endTime - startTime;
-                System.out.println("Execution time in milliseconds: "+ timeElapsed /1000000);
-                break;
-            }
-            case 2: {
-                long startTime = System.nanoTime();
-                DictionaryService service = new DictionaryService(DictionaryType.HASH_SET);
-                service.findWordsFile("bible.txt");
-                long endTime = System.nanoTime();
-                long timeElapsed = endTime - startTime;
-                System.out.println("Execution time in milliseconds: "+ timeElapsed /1000000);
-                break;
-            }
-            case 3: {
-                long startTime = System.nanoTime();
-                DictionaryService service = new DictionaryService(DictionaryType.TREE_SET);
-                service.findWordsFile("bible.txt");
-                long endTime = System.nanoTime();
-                long timeElapsed = endTime - startTime;
-                System.out.println("Execution time in milliseconds: "+ timeElapsed /1000000);
-                break;
-            }
-        }
+        long startTime = System.nanoTime();
+        DictionaryService service = new DictionaryService(DictionaryType.ARRAY_LIST);
+        service.findWordsFile("bible.txt");
+        long endTime = System.nanoTime();
+        long timeElapsed = endTime - startTime;
+        System.out.println("Execution time in milliseconds: "+ timeElapsed /1000000);
+
+        long startTime1 = System.nanoTime();
+        DictionaryService service1 = new DictionaryService(DictionaryType.HASH_SET);
+        service.findWordsFile("bible.txt");
+        long endTime1 = System.nanoTime();
+        long timeElapsed1 = endTime - startTime;
+        System.out.println("Execution time in milliseconds: "+ timeElapsed1 /1000000);
+
+        long startTime2 = System.nanoTime();
+        DictionaryService service2 = new DictionaryService(DictionaryType.TREE_SET);
+        service.findWordsFile("bible.txt");
+        long endTime2 = System.nanoTime();
+        long timeElapsed2 = endTime - startTime;
+        System.out.println("Execution time in milliseconds: "+ timeElapsed2 /1000000);
     }
 }
